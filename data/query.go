9a2e95f8d6818b766e0f292280993819db7ce771
nzai <nzai@16.3com>
2015-11-18 00:25:15 +0800

    配合服务器端的序列化格式，修正查询方法

--- data/query.go
+++ data/query.go
@@ -16,7 +16,8 @@ func QueryPeroids(market, code, start, end string) ([]m.Peroid60, error) {
 
 	log.Print("ServerAddress:", config.Get().ServerAddress)
 	//	url := path.Join(config.Get().ServerAddress, market, code, start, end, "1m")
-	url := "http://52.69.228.175:602/america/aapl/20151101/20151111/1m"
+	//	url := "http://52.69.228.175:602/america/aapl/20151101/20151111/1m"
+	url := "http://localhost:602/america/aapl/20151101/20151111/1m"
 	log.Print("url:", url)
 
 	content, err := io.DownloadString(url)
@@ -24,7 +25,7 @@ func QueryPeroids(market, code, start, end string) ([]m.Peroid60, error) {
 		return nil, err
 	}
 
-	log.Print("content:", content)
+	//log.Print("content:", content)
 
 	r := result.HttpResult{}
 	err = json.Unmarshal([]byte(content), &r)
@@ -36,10 +37,37 @@ func QueryPeroids(market, code, start, end string) ([]m.Peroid60, error) {
 		return nil, fmt.Errorf("从服务器查询分时数据出错:%s", r.Message)
 	}
 
-	objs := r.Data.([]interface{})
+	objs, ok := r.Data.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("转换Data出错:%v", r.Data)
+	}
+
+	var timeString string
+	var open, _close, high, low float32
+	var volume int64
+
 	peroids := make([]m.Peroid60, 0)
 	for _, obj := range objs {
-		peroids = append(peroids, obj.(m.Peroid60))
+
+		str, ok := obj.(string)
+		if !ok {
+			return nil, fmt.Errorf("转换Data出错:%v", obj)
+		}
+
+		_, err := fmt.Sscanf(str, "%s %f %f %f %f %d", &timeString, &open, &_close, &high, &low, &volume)
+		if err != nil {
+			return nil, fmt.Errorf("转换Peroid60出错:%s", err.Error())
+		}
+
+		peroids = append(peroids, m.Peroid60{
+			Market: market,
+			Code:   code,
+			Time:   timeString,
+			Open:   open,
+			Close:  _close,
+			High:   high,
+			Low:    low,
+			Volume: volume})
 	}
 
 	return peroids, nil
