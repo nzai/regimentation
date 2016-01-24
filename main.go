66688b96b0822f0d373b6dc45326d737b1cb5533
nzai <nzai@16.3com>
2016-01-25 01:16:26 +0800

    添加测试

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/nzai/regimentation/config"
 	"github.com/nzai/regimentation/data"
@@ -28,7 +29,18 @@ func main() {
 		return
 	}
 
-	peroids, err := data.QueryPeroids("america", "aapl", "20151101", "20151111")
+	start, err := time.Parse("20060102", "20151101")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	end, err := time.Parse("20060102", "20151111")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	peroids, err := data.QueryPeroids("america", "aapl", start, end)
 	if err != nil {
 		log.Fatal("查询服务器数据出错: ", err)
 		return
