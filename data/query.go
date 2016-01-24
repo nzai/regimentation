663d5ed81eb276159918b9a8ddcea2fa888d4ae8
nzai <nzai@16.3com>
2016-01-25 01:16:47 +0800

    调整查询接口和数据结构

--- data/query.go
+++ data/query.go
@@ -4,23 +4,55 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/nzai/go-utility/net"
+	utime "github.com/nzai/go-utility/time"
 	"github.com/nzai/regimentation/config"
-	m "github.com/nzai/stockrecorder/market"
 	"github.com/nzai/stockrecorder/server/result"
 )
 
+//	每分钟历史
+type MinuteHistory struct {
+	Market string
+	Code   string
+	Time   time.Time
+	Open   float32
+	Close  float32
+	High   float32
+	Low    float32
+	Volume int64
+}
+
+//	公司列表
+type MinuteHistories []MinuteHistory
+
+func (l MinuteHistories) Len() int {
+	return len(l)
+}
+func (l MinuteHistories) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+func (l MinuteHistories) Less(i, j int) bool {
+	return l[i].Time.Before(l[j].Time)
+}
+
 //	查询分时数据
-func QueryPeroids(market, code, start, end string) ([]m.Peroid60, error) {
+func QueryMinuteHistories(market, code string, start, end time.Time) ([]MinuteHistory, error) {
 
 	log.Print("ServerAddress:", config.Get().ServerAddress)
-	//	url := path.Join(config.Get().ServerAddress, market, code, start, end, "1m")
+	//		url := path.Join(config.Get().ServerAddress, market, code, start, end, "1m")
+	url := fmt.Sprintf("%s/%s/%s/%s/%s/1m",
+		config.Get().ServerAddress,
+		strings.ToLower(market),
+		strings.ToLower(code),
+		start.Format("20060102"),
+		end.Format("20060102"))
 	//	url := "http://52.69.228.175:602/america/aapl/20151101/20151111/1m"
-	url := "http://localhost:602/america/aapl/20151101/20151111/1m"
+	//	url := "http://localhost:602/america/aapl/20151101/20151111/1m"
 	log.Print("url:", url)
 	content, err := net.DownloadString(url)
 	if err != nil {
@@ -45,7 +77,7 @@ func QueryPeroids(market, code, start, end string) ([]m.Peroid60, error) {
 	upperMarket := strings.Title(market)
 	upperCode := strings.ToUpper(code)
 
-	peroids := make([]m.Peroid60, 0)
+	histories := make([]MinuteHistory, 0)
 	for _, obj := range objs {
 		//log.Print(reflect.TypeOf(obj).String())
 		values, ok := obj.([]interface{})
@@ -58,7 +90,7 @@ func QueryPeroids(market, code, start, end string) ([]m.Peroid60, error) {
 			return nil, err
 		}
 
-		peroids = append(peroids, m.Peroid60{
+		histories = append(histories, MinuteHistory{
 			Market: upperMarket,
 			Code:   upperCode,
 			Time:   _time,
@@ -69,5 +101,73 @@ func QueryPeroids(market, code, start, end string) ([]m.Peroid60, error) {
 			Volume: int64(values[5].(float64))})
 	}
 
-	return peroids, nil
+	return histories, nil
+}
+
+//	每日历史
+type DayHistory struct {
+	Market string
+	Code   string
+	Date   time.Time
+	Open   float32
+	Close  float32
+	High   float32
+	Low    float32
+	Volume int64
+}
+
+func ParseDayHistory(minutes []MinuteHistory) ([]DayHistory, error) {
+	if len(minutes) == 0 {
+		return nil, fmt.Errorf("minutes为空")
+	}
+
+	//	分钟数据排序
+	minuteHistories := MinuteHistories(minutes)
+	sort.Sort(minuteHistories)
+	minutes = []MinuteHistory(minuteHistories)
+
+	var current DayHistory
+	var tomorrow time.Time
+
+	list := make([]DayHistory, 0)
+	for index, minute := range minutes {
+
+		if index == 0 || !minute.Time.Before(tomorrow) {
+			if index > 0 {
+				//	记录
+				list = append(list, current)
+			}
+
+			current = DayHistory{
+				Market: minute.Market,
+				Code:   minute.Code,
+				Date:   utime.BeginOfDay(minute.Time),
+				Open:   minute.Open,
+				High:   minute.High,
+				Low:    minute.Low,
+				Volume: minute.Volume}
+
+			tomorrow = current.Date.Add(time.Hour * 24)
+
+			continue
+		}
+
+		if minute.High > current.High {
+			current.High = minute.High
+		}
+
+		if minute.Low < current.Low {
+			current.Low = minute.Low
+		}
+
+		current.Close = minute.Close
+		current.Volume += minute.Volume
+	}
+
+	//	记录最后一天的数据
+	if list[len(list)-1].Date.Before(current.Date) {
+		list = append(list, current)
+	}
+
+	return list, nil
 }
