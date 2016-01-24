53a7d239d207164dbee525c9d124bea80119cefa
nzai <nzai@16.3com>
2016-01-25 01:18:25 +0800

    头寸定义

---
+++ trading/holding.go
@@ -0,0 +1,14 @@
+package trading
+
+import "time"
+
+//	头寸
+type Holding struct {
+	StartTime  time.Time //	起始时间
+	StartPrice float32   //	起始价格
+	Direction  bool      //	做多
+	Quantity   int       //	数量
+	EndTime    time.Time //	结束时间
+	EndPrice   float32   //	结束价格
+	Profit     float32   //	利润
+}
