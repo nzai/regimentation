d90694b869f7effedfcb5c31a3304e336e11f9d9
nzai <nzai@16.3com>
2016-02-17 01:18:11 +0800

    调整配置读取方式，并增加了一些测试代码

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"time"
 
-	"github.com/nzai/go-utility/path"
 	"github.com/nzai/regimentation/config"
 
 	"github.com/nzai/regimentation/trading"
@@ -19,19 +18,13 @@ func main() {
 		}
 	}()
 
-	root, err := path.GetStartupDir()
-	if err != nil {
-		log.Fatal("获取起始目录失败: ", err)
-		return
-	}
-
 	//	读取配置文件
-	err = config.SetRootDir(root)
+	err := config.ReadConfig()
 	if err != nil {
 		log.Fatal("读取配置文件错误: ", err)
 	}
 
-	start, err := time.Parse("20060102", "20151101")
+	start, err := time.Parse("20060102", "20151001")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,61 +52,36 @@ func main() {
 	//	演算
 	system.Simulate()
 
-	//	新的测试
-	//	newTest := &trading.TurtleSystemTest{
-	//		TurtleSystem: &system,
-	//		TurtleSetting: &trading.TurtleSetting{
-	//			Holding: 2,
-	//			N:       2,
-	//			Enter:   2,
-	//			Exit:    2,
-	//			Stop:    2},
-	//		StartAmount: system.StartAmount,
-	//		EndAmount:   system.StartAmount,
-	//		Trends:      []trading.Trend{}}
+	//	go system.SimulateResultProcess()
 
-	//	//	演算
-	//	err = newTest.Simulate()
-	//	if err != nil {
-	//		log.Printf("[Error]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Error:%s",
-	//			newTest.TurtleSetting.Holding,
-	//			newTest.TurtleSetting.N,
-	//			newTest.TurtleSetting.Enter,
-	//			newTest.TurtleSetting.Exit,
-	//			newTest.TurtleSetting.Stop,
-	//			err.Error())
-	//	}
+	//	system.SimulateOnce(system.MinSetting)
+	//	log.Printf("Profit:%.3f", system.CurrentProfit)
 
-	//	for _, trend := range newTest.Trends {
-
-	//		direction := "做多"
-	//		if !trend.Direction {
-	//			direction = "做空"
+	//	time.Sleep(time.Second * 5)
+	//	极值测试
+	//	for index, history := range system.MinutePeroids {
+	//		if index >= 40 {
+	//			break
 	//		}
 
-	//		var quantity int = 0
-	//		for _, holding := range trend.Holdings {
-	//			quantity += holding.Quantity
+	//		//		pe, err := system.PeroidExtermaIndexes.Get(4, history.Time)
+	//		//		if err != nil {
+	//		//			log.Fatal(err)
+	//		//		}
+
+	//		//		log.Printf("%s %.3f %.3f  [4]  %.3f %.3f", history.Time.Format("2006-01-02 15:04"), history.High, history.Low, pe.Max, pe.Min)
+
+	//		tu, err := system.TurtleIndexes.Get(4, history.Time)
+	//		if err != nil {
+	//			log.Fatal(err)
 	//		}
 
-	//		log.Printf("%s %s %s %s %s 数量:%d 利润:%.2f",
-	//			trend.StartTime.Format("2006-01-02 15:04"),
-	//			trend.StartReason,
-	//			direction,
-	//			trend.EndTime.Format("2006-01-02 15:04"),
-	//			trend.EndReason,
-	//			quantity,
-	//			trend.Profit)
+	//		log.Printf("%s %.3f %.3f %.3f  [4]  %.3f %.3f",
+	//			history.Time.Format("2006-01-02 15:04"),
+	//			history.High,
+	//			history.Low,
+	//			history.Close,
+	//			tu.TR,
+	//			tu.N)
 	//	}
-
-	//	log.Print(len(newTest.Trends))
-
-	//	log.Printf("[Current]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f",
-	//		newTest.TurtleSetting.Holding,
-	//		newTest.TurtleSetting.N,
-	//		newTest.TurtleSetting.Enter,
-	//		newTest.TurtleSetting.Exit,
-	//		newTest.TurtleSetting.Stop,
-	//		newTest.Profit,
-	//		newTest.ProfitPercent*100)
 }
