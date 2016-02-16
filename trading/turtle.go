742c3feee6c261209f071f319dbe4040bc9f6c22
nzai <nzai@16.3com>
2016-02-17 01:17:34 +0800

    将不同参数的演算过程改为并行方式以提高计算效率

--- trading/turtle.go
+++ trading/turtle.go
@@ -1,11 +1,18 @@
 package trading
 
 import (
+	"fmt"
 	"log"
 	"math"
+	"sync"
 	"time"
 
 	"github.com/nzai/regimentation/data"
+)
+
+const (
+	SimulateGCCount     = 32
+	ProgressDelaySecond = 5
 )
 
 //	海龟设定
@@ -15,6 +22,13 @@ type TurtleSetting struct {
 	Enter   int //	入市区间
 	Exit    int //	退市区间
 	Stop    int //	止损区间
+}
+
+//	海龟演算结果
+type TurtleSimulateResult struct {
+	Setting       TurtleSetting //	海龟设定
+	Profit        float32       //	利润
+	ProfitPercent float32       //	利润率
 }
 
 //	海龟系统
@@ -44,6 +58,8 @@ type TurtleSystem struct {
 
 	PeroidExtermaIndexes *data.PeroidExtermaIndexes //	区间极值
 	TurtleIndexes        *data.TurtleIndexes        //	海龟指标
+
+	simulateResultChannel chan TurtleSimulateResult //	验算结果发送通道
 }
 
 //	初始化
@@ -75,7 +91,6 @@ func (t *TurtleSystem) Init() error {
 		peroids = append(peroids, peroid)
 	}
 	//	log.Printf("peroids:%v", peroids)
-
 	t.PeroidExtermaIndexes = &data.PeroidExtermaIndexes{}
 	t.PeroidExtermaIndexes.Init(t.MinutePeroids, peroids...)
 
@@ -87,13 +102,15 @@ func (t *TurtleSystem) Init() error {
 	//	log.Printf("ns:%v", ns)
 	t.TurtleIndexes = &data.TurtleIndexes{}
 	t.TurtleIndexes.Init(t.MinutePeroids, ns...)
-
 	t.TotalAmount = (t.MaxSetting.Holding - t.MinSetting.Holding + 1) *
 		(t.MaxSetting.N - t.MinSetting.N + 1) *
 		(t.MaxSetting.Enter - t.MinSetting.Enter + 1) *
 		(t.MaxSetting.Exit - t.MinSetting.Exit + 1) *
 		(t.MaxSetting.Stop - t.MinSetting.Stop + 1)
 	t.Caculated = 0
+
+	//	初始化演算结果通道
+	t.simulateResultChannel = make(chan TurtleSimulateResult)
 
 	log.Print("初始化结束")
 
@@ -108,13 +125,97 @@ func (t *TurtleSystem) Simulate() {
 	t.EndAmount = t.StartAmount
 	t.BestProfit = -math.MaxFloat32
 
-	go func() {
-		//	每天运行一次
-		ticker := time.NewTicker(time.Second * 120)
+	//	进度显示
+	go t.simulateProgress()
 
-		for _ = range ticker.C {
+	//	演算结果处理
+	go t.SimulateResultProcess()
 
-			log.Printf("[Current]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f\t%d %d %.2f",
+	defer close(t.simulateResultChannel)
+
+	chanSend := make(chan int, SimulateGCCount)
+	defer close(chanSend)
+
+	var wg sync.WaitGroup
+	wg.Add(t.TotalAmount)
+
+	for stop := t.MinSetting.Stop; stop <= t.MaxSetting.Stop; stop++ {
+		for exit := t.MinSetting.Exit; exit <= t.MaxSetting.Exit; exit++ {
+			for enter := t.MinSetting.Enter; enter <= t.MaxSetting.Enter; enter++ {
+				for n := t.MinSetting.N; n <= t.MaxSetting.N; n++ {
+					for holding := t.MinSetting.Holding; holding <= t.MaxSetting.Holding; holding++ {
+
+						//	并发演算
+						go func(setting TurtleSetting) {
+							_err := t.SimulateOnce(setting)
+							if _err != nil {
+								log.Print(_err.Error())
+							}
+
+							<-chanSend
+							wg.Done()
+						}(TurtleSetting{Holding: holding, N: n, Enter: enter, Exit: exit, Stop: stop})
+
+						chanSend <- 1
+					}
+				}
+			}
+		}
+	}
+
+	//	阻塞，直到演算完所有组合
+	wg.Wait()
+
+	log.Printf("[Simulate]\t演算结束,最佳设定Holding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f",
+		t.BestSetting.Holding,
+		t.BestSetting.N,
+		t.BestSetting.Enter,
+		t.BestSetting.Exit,
+		t.BestSetting.Stop,
+		t.BestProfit,
+		t.BestProfitPercent*100)
+}
+
+//	演算一种配置
+func (t *TurtleSystem) SimulateOnce(setting TurtleSetting) error {
+
+	//	新的测试
+	newTest := &TurtleSystemTest{
+		TurtleSystem:  t,
+		TurtleSetting: &setting,
+		StartAmount:   t.StartAmount,
+		EndAmount:     t.StartAmount,
+		Trends:        []Trend{}}
+
+	//	演算
+	err := newTest.Simulate()
+	if err != nil {
+		return fmt.Errorf("[Error]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Error:%s",
+			t.CurrentSetting.Holding,
+			t.CurrentSetting.N,
+			t.CurrentSetting.Enter,
+			t.CurrentSetting.Exit,
+			t.CurrentSetting.Stop,
+			err.Error())
+	}
+
+	//	发送演算结果
+	t.simulateResultChannel <- TurtleSimulateResult{
+		Setting:       setting,
+		Profit:        newTest.Profit,
+		ProfitPercent: newTest.ProfitPercent}
+
+	return nil
+}
+
+//	演算进度
+func (t *TurtleSystem) simulateProgress() {
+	//	定时任务
+	ticker := time.NewTicker(time.Second * ProgressDelaySecond)
+
+	for _ = range ticker.C {
+		if t.CurrentSetting != nil {
+			log.Printf("[Current]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f\t%d %d %03.2f%%",
 				t.CurrentSetting.Holding,
 				t.CurrentSetting.N,
 				t.CurrentSetting.Enter,
@@ -126,67 +227,47 @@ func (t *TurtleSystem) Simulate() {
 				t.TotalAmount,
 				float32(t.Caculated)/float32(t.TotalAmount)*100)
 		}
-	}()
 
-	for stop := t.MinSetting.Stop; stop <= t.MaxSetting.Stop; stop++ {
-		for exit := t.MinSetting.Exit; exit <= t.MaxSetting.Exit; exit++ {
-			for enter := t.MinSetting.Enter; enter <= t.MaxSetting.Enter; enter++ {
-				for n := t.MinSetting.N; n <= t.MaxSetting.N; n++ {
-					for holding := t.MinSetting.Holding; holding <= t.MaxSetting.Holding; holding++ {
-
-						t.CurrentSetting = &TurtleSetting{
-							Holding: holding,
-							N:       n,
-							Enter:   enter,
-							Exit:    exit,
-							Stop:    stop}
-
-						//	新的测试
-						newTest := &TurtleSystemTest{
-							TurtleSystem:  t,
-							TurtleSetting: t.CurrentSetting,
-							StartAmount:   t.StartAmount,
-							EndAmount:     t.StartAmount,
-							Trends:        []Trend{}}
-
-						//	演算
-						err := newTest.Simulate()
-						if err != nil {
-							log.Printf("[Error]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Error:%s",
-								t.CurrentSetting.Holding,
-								t.CurrentSetting.N,
-								t.CurrentSetting.Enter,
-								t.CurrentSetting.Exit,
-								t.CurrentSetting.Stop,
-								err.Error())
-						}
-
-						if t.CurrentProfit > t.BestProfit {
-							t.BestSetting = t.CurrentSetting
-							t.BestProfit = t.CurrentProfit
-							t.BestProfitPercent = t.CurrentProfitPercent
-
-							log.Printf("[Best]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f",
-								t.BestSetting.Holding,
-								t.BestSetting.N,
-								t.BestSetting.Enter,
-								t.BestSetting.Exit,
-								t.BestSetting.Stop,
-								t.BestProfit,
-								t.BestProfitPercent*100)
-						}
-					}
-				}
-			}
+		if t.BestSetting != nil {
+			log.Printf("[Best]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f",
+				t.BestSetting.Holding,
+				t.BestSetting.N,
+				t.BestSetting.Enter,
+				t.BestSetting.Exit,
+				t.BestSetting.Stop,
+				t.BestProfit,
+				t.BestProfitPercent*100)
 		}
-	}
 
-	log.Printf("[Simulate]\t演算结束,最佳设定Holding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f",
-		t.BestSetting.Holding,
-		t.BestSetting.N,
-		t.BestSetting.Enter,
-		t.BestSetting.Exit,
-		t.BestSetting.Stop,
-		t.BestProfit,
-		t.BestProfitPercent*100)
+	}
+}
+
+//	演算结果处理
+func (t *TurtleSystem) SimulateResultProcess() {
+
+	for {
+		//	从通道中读取演算结果
+		result := <-t.simulateResultChannel
+
+		t.CurrentSetting = &result.Setting
+		t.CurrentProfit = result.Profit
+		t.CurrentProfitPercent = result.ProfitPercent
+
+		if t.CurrentProfit > t.BestProfit {
+			t.BestSetting = t.CurrentSetting
+			t.BestProfit = t.CurrentProfit
+			t.BestProfitPercent = t.CurrentProfitPercent
+
+			log.Printf("[Best]\tHolding:%d N:%d Enter:%d Exit:%d Stop:%d Profit:%f ProfitPercent:%.3f",
+				t.BestSetting.Holding,
+				t.BestSetting.N,
+				t.BestSetting.Enter,
+				t.BestSetting.Exit,
+				t.BestSetting.Stop,
+				t.BestProfit,
+				t.BestProfitPercent*100)
+		}
+
+		t.Caculated++
+	}
 }
